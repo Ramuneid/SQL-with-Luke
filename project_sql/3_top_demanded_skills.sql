7935d0747a7b9155d4cb5739ad7862c77b892794
Ramunė Idzelytė <r.s.m.idzelyte@gmail.com>
2025-12-23 11:50:18 +0200

    Update 3_top_demanded_skills.sql

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,4 +1,4 @@
-/**Question: What are the most in-demand skills for data analysts?**
+/*Question: What are the most in-demand skills for data analysts in Lithuania?
 - Identify the top 5 in-demand skills for a data analyst.
 - Focus on all job postings.
 - Why? Retrieves the top 5 skills with the highest demand in the job market, providing insights into the most valuable skills for job seekers.*/
