f5d67ed8efe549cf547b594c547dd155c6ab542a
Ramunė Idzelytė <r.s.m.idzelyte@gmail.com>
2025-12-23 11:41:38 +0200

    Delete sql_course_bonus.session.sql

--- sql_course_bonus.session.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    job_via,
-    COUNT (job_id) AS darbo_platforma
-FROM job_postings_fact
-WHERE job_country = 'Lithuania' AND EXTRACT( YEAR FROM job_posted_date) = 2025
-GROUP BY job_via
-ORDER BY darbo_platforma DESC;
